a2ba6a41af7833deaefe2dbb8b209f56e0b67234
Nathan764 <nathanoli330@gmail.com>
2022-03-30 17:25:36 -0300

    pokedex

---
+++ funcoes.js
@@ -0,0 +1,27 @@
+const fetchPokemon = () =>{
+    const pokemonPromises = []
+    let html = ""
+
+    for(let i = 1; i<=150; i++){
+    let url ='https://pokeapi.co/api/v2/pokemon/' + i
+    
+     
+    pokemonPromises.push(
+        fetch(url)
+        .then(response => response.json())
+    )
+}
+   Promise.all(pokemonPromises)
+    .then(pokemon =>{
+        pokemon.forEach(pokemon => {
+            html += `
+            <li class="cartao">
+            <h2 class="cartao-titulo">${pokemon.id} - ${pokemon.name}</h2>
+            <img src="${pokemon.sprites.front_default}">
+            </li>
+            `
+        });
+        document.querySelector(".pokedex").innerHTML = html
+    })
+ }
+fetchPokemon()
